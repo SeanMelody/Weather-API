5d3ba7c56e15e57b142346d2e73b1ded0b856dc5
Sean Melody <seandmelody@gmail.com>
2020-10-30 16:52:33 -0700

    Styling changes

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var weatherMain = document.querySelector(".weather-main");
 var fetchButton = document.querySelector(".get-weather");
 
 // function getWeather() {
-var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?zip=94965&units=imperial&cnt=6&appid=fd7013d34fa65ca951cba9b9f0dde107';
+// var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?zip=94965&units=imperial&cnt=6&appid=fd7013d34fa65ca951cba9b9f0dde107';
 
 fetch(requestUrl, {
 
@@ -49,18 +49,80 @@ fetch(requestUrl, {
         //     issueContainer.append(issueBody);
         // }
 
-        var dayOne = document.querySelector(".day-one");
-        var dayTwo = document.querySelector(".day-two");
-        var dayThree = document.querySelector(".day-three");
-        var dayFour = document.querySelector(".day-four");
-        var dayFive = document.querySelector(".day-five");
+        // DAY 1 of 5
+        // var dayOneTemp = document.querySelector("#day-one-temp");
+        // var dayTwoTemp = document.querySelector("#day-two-temp");
+        // var dayThreeTemp = document.querySelector("#day-three-temp");
+        // var dayFourTemp = document.querySelector("#day-four-temp");
+        // var dayFiveTemp = document.querySelector("#day-five-temp");
+        var dayOneDate = document.querySelector("#day-one-date");
+        var dayOneDescription = document.querySelector("#day-one-description");
+        var dayOneIcon = document.querySelector("#day-one-icon");
+        var dayOneTemp = document.querySelector("#day-one-temp");
+        var dayOneHumidity = document.querySelector("#day-one-humidity");
 
-        dayOne.textContent = data.list[1].main.temp
-        dayTwo.textContent = data.list[2].main.temp
-        dayThree.textContent = data.list[3].main.temp
-        dayFour.textContent = data.list[4].main.temp
-        dayFive.textContent = data.list[5].main.temp
+        dayOneDate.textContent = data.list[0].dt_txt
+        dayOneDescription.textContent = data.list[0].weather[0].description
+        dayOneIcon.textContent = data.list[0].weather[0].icon
+        dayOneTemp.textContent = data.list[0].main.temp
+        dayOneHumidity.textContent = data.list[0].main.humidity
+
+        // dayTwoTemp.textContent = data.list[2].main.temp
+        // dayThreeTemp.textContent = data.list[3].main.temp
+        // dayFourTemp.textContent = data.list[4].main.temp
+        // dayFiveTemp.textContent = data.list[5].main.temp
+
+        // DAY 2 of 5
+        var dayTwoDate = document.querySelector("#day-two-date");
+        var dayTwoIcon = document.querySelector("#day-two-icon");
+        var dayTwoTemp = document.querySelector("#day-two-temp");
+        var dayTwoHumidity = document.querySelector("#day-two-humidity");
+
+        dayTwoDate.textContent = data.list[1].dt_txt
+        // dayTwoDescription.textContent = data.list[1].weather[0].description
+        dayTwoIcon.textContent = data.list[1].weather[0].icon
+        dayTwoTemp.textContent = data.list[1].main.temp
+        dayTwoHumidity.textContent = data.list[1].main.humidity
+
+
+        // DAY 3 of 5
+        var dayThreeDate = document.querySelector("#day-three-date");
+        var dayThreeIcon = document.querySelector("#day-three-icon");
+        var dayThreeTemp = document.querySelector("#day-three-temp");
+        var dayThreeHumidity = document.querySelector("#day-three-humidity");
+
+        dayThreeDate.textContent = data.list[2].dt_txt
+        // dayThreeDescription.textContent = data.list[2].weather[0].description
+        dayThreeIcon.textContent = data.list[2].weather[0].icon
+        dayThreeTemp.textContent = data.list[2].main.temp
+        dayThreeHumidity.textContent = data.list[2].main.humidity
+
+        // DAY 4 of 5
+        var dayFourDate = document.querySelector("#day-four-date");
+        var dayFourIcon = document.querySelector("#day-four-icon");
+        var dayFourTemp = document.querySelector("#day-four-temp");
+        var dayFourHumidity = document.querySelector("#day-four-humidity");
+
+        dayFourDate.textContent = data.list[3].dt_txt
+        // dayFourDescription.textContent = data.list[3].weather[0].description
+        dayFourIcon.textContent = data.list[3].weather[0].icon
+        dayFourTemp.textContent = data.list[3].main.temp
+        dayFourHumidity.textContent = data.list[3].main.humidity
+
+        // DAY 5 of 5
+        var dayFiveDate = document.querySelector("#day-five-date");
+        var dayFiveIcon = document.querySelector("#day-five-icon");
+        var dayFiveTemp = document.querySelector("#day-five-temp");
+        var dayFiveHumidity = document.querySelector("#day-five-humidity");
+
+        dayFiveDate.textContent = data.list[4].dt_txt
+        // dayFiveDescription.textContent = data.list[4].weather[0].description
+        dayFiveIcon.textContent = data.list[4].weather[0].icon
+        dayFiveTemp.textContent = data.list[4].main.temp
+        dayFiveHumidity.textContent = data.list[4].main.humidity
+
     })
+    // })
 // }
 
 
